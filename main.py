d1272331edf27cbed7f219be27433f28863dcc65
Mauricioqm <quinonesmauricio53@gmail.com>
2023-09-16 17:02:30 -0500

    Prueba python

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
 import random
 
 usuarios = []
+prestamosUsuarios = []
+bicicletas = ["false", "true", "false", "false"]
 accion = 0
-codigo = 0
+numeroTarjeta = 0
 origen = ''
 destino = ''
 
-def viaje () :
-  origen = str(input("Ingrese el origen: "))
-  destino = str(input("Ingrese el destino: "))
-  codigo = round(random.uniform(1, 1000000))
-  if origen != '' and destino != '' :
-    print(f"El codigo para su viaje de {origen} a {destino} es el {codigo}")
-  else :
-      print("Ingrese un origen y un destino")  
+def viaje (numeroTarjeta) :
+    tarjeta = int(input('Ingreso el numero tarjeta: '))
+    origen = str(input("Ingrese el origen: "))
+    destino = str(input("Ingrese el destino: "))
+    if origen != '' and destino != '' and tarjeta == numeroTarjeta :
+        if 'true' in bicicletas :
+            prestamosUsuarios.append({'origen': origen, 'destino': destino})
+            print(f"Reserva exitosa")
+        else :
+            print(f"No hay bicicletas disponibles")
+    else :
+        print(f"Ingrese un origen y un destino y verifique el número de tarjeta") 
+
+
+def menuLogin(tarjeta) :
+    accionesLogin = 0
+    accionesLogin = int(input("Ingrese una opcion: \n1. Consultar usuarios \n2. Consultar prestamos \n3. Prestar bicicleta  \n4. Salir "))
+    while accionesLogin != 4 :
+        if accionesLogin == 1 :
+            for usuario in usuarios :
+                print(usuario)
+                break
+        elif accionesLogin == 2 :
+            for prestamo in prestamosUsuarios :
+                print(prestamo)
+        elif accionesLogin == 3 :
+            viaje(tarjeta)
+        elif accionesLogin == 4 :
+            print('Gracias')
+            break  
+                   
 
 def login(documento, password):
     usuario = {
         'id': documento,
         'pass': password
     }
-    if usuario in usuarios:
-        print("Bienvenido")
-        viaje()
-    else :
-        print("Usuario o contraseña incorrectos")
+    for usuario in usuarios :
+        if usuario['id'] == documento and usuario['pass'] == password:
+            print("Bienvenido")
+            menuLogin(usuario['numeroTarjeta'])
+        else :
+            print("Usuario o contraseña incorrectos")
             
             
 def registro(documento, password):
@@ -35,8 +61,14 @@ def registro(documento, password):
     if usuarioNuevo in usuarios:
         print("El usuario ya ha sido creado")
     else :
-        print("El usuario se ha creado exitosamente")
+        tarjeta  = round(random.uniform(1, 100))
+        usuarioNuevo = {
+            'id': documento,
+            'pass': password,
+            'numeroTarjeta': tarjeta
+        }
         usuarios.append(usuarioNuevo)
+        print(f"El usuario se ha creado exitosamente, su tarjeta es la {tarjeta}")
 
 
 while accion!= 3:
